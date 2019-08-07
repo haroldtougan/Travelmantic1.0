c1e438cc9d53e4dc30c0026eb7bf1bcc7383ca83
harold tougan <haroldhenr@gmail.com>
2019-08-07 16:15:18 +0000

    ALC Travelmantics 4.0 project 2

---
+++ app/src/main/java/com/example/travelmantics/FirebaseUtil.java
@@ -0,0 +1,98 @@
+package com.example.travelmantics;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+
+public class FirebaseUtil {
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private static FirebaseUtil firebaseUtil;
+    public static FirebaseAuth mfirebaseAuth;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
+    public static ArrayList<TravelDeal> mDeals;
+    private static final int RC_SIGN_IN = 123;
+    private static Activity caller;
+    private FirebaseUtil() {
+    }
+
+
+
+    public static void openFbReference(String ref, final Activity callerActivity) {
+        if (firebaseUtil == null) {
+            firebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mfirebaseAuth = FirebaseAuth.getInstance();
+            caller = callerActivity;
+            mAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    if (firebaseAuth.getCurrentUser() == null) {
+                        FirebaseUtil.signIn();
+                    }
+                    else {
+                        String userId = firebaseAuth.getUid();
+
+                    }
+                    FirebaseUtil.signIn();
+                    Toast.makeText(callerActivity.getBaseContext(),"Welcome Back!", Toast.LENGTH_LONG).show();
+
+
+                }
+            };
+
+        }
+        mDeals = new ArrayList<TravelDeal>();
+
+        mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+
+    }
+    private static void signIn () {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+
+
+// Create and launch sign-in intent
+        caller.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+
+    public static void attachListener() {
+        mfirebaseAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+
+    public static void detachListener() {
+        mfirebaseAuth.removeAuthStateListener(mAuthListener);
+
+    }
+
+}
+
+
+
+
+
+
+
